cfde3b95155d539879ba65927983dfadf62ac3ea
Obrepal <48407260+Obrepal@users.noreply.github.com>
2019-11-30 09:06:22 +0100

    cleaning

--- src/main/java/com/game/enitity/creature/Enemy.java
+++ src/main/java/com/game/enitity/creature/Enemy.java
@@ -31,49 +31,40 @@ public class Enemy extends Creature {
         xMove = 0;
         yMove = 0;
 
-        if ((y < Player.getYP() && checkDown() && x >= Player.getXP()) || ( x >= Player.getXP() && !checkLeft())) //up
-            yMove = (speed -1);
+        if (( y < Player.getYP() && checkDown() && x >= Player.getXP() ) || ( x >= Player.getXP() && !checkLeft() )) //up
+            yMove = ( speed -1 );
 
-        if((y > Player.getYP() && checkUP() && x <= Player.getXP()) ||  (x <= Player.getXP() && !checkRight())) // down
-            yMove = (-speed+1);
+        if(( y > Player.getYP() && checkUP() && x <= Player.getXP() ) || ( x <= Player.getXP() && !checkRight() )) // down
+            yMove = ( -speed+1 );
 
-        if((x > Player.getXP() && checkLeft() && y >= Player.getYP() ) || (y >= Player.getYP() && !checkUP()) ) // left
-         xMove = (-speed + 1);
+        if( (x > Player.getXP() && checkLeft() && y >= Player.getYP() ) || ( y >= Player.getYP() && !checkUP() )) // left
+            xMove = ( -speed + 1 );
 
-       if((x < Player.getXP() && checkRight() && y <= Player.getYP() ) || (y <= Player.getYP() && !checkDown()) ) //right
-            xMove = (speed-1);
+       if(( x < Player.getXP() && checkRight() && y <= Player.getYP() ) || ( y <= Player.getYP() && !checkDown() )) //right
+            xMove = ( speed-1 );
     }
 
-
- //CHECKING BOUNDINGBOX ALL TOGETHER true can go
-
+    //CHECKING BOUNDINGBOX
     private boolean checkUP(){
         int ty = (int) (y - 3 + bounds.y) / Tile.TILEHEIGHT;
          return (!collisionWithTile2((int) (x + bounds.x) / Tile.TILEWIDTH, ty) &&
                 !collisionWithTile2((int) (x + bounds.x + bounds.width) / Tile.TILEWIDTH, ty));
-
     }
-
-   private boolean checkDown(){
-        int ty = (int) (y +3 + bounds.y + bounds.height) / Tile.TILEHEIGHT;
+    private boolean checkDown(){
+        int ty = (int) (y + 3 + bounds.y + bounds.height) / Tile.TILEHEIGHT;
         return (!collisionWithTile2((int) (x + bounds.x) / Tile.TILEWIDTH, ty) &&
                 !collisionWithTile2((int) (x + bounds.x + bounds.width) / Tile.TILEWIDTH, ty));
     }
-
-  private   boolean checkLeft(){
-        int tx = (int) (x -3 + bounds.x) / Tile.TILEWIDTH;
+    private   boolean checkLeft(){
+        int tx = (int) (x - 3 + bounds.x) / Tile.TILEWIDTH;
         return (!collisionWithTile2(tx, (int) (y + bounds.y) / Tile.TILEHEIGHT) &&
                 !collisionWithTile2(tx, (int) (y + bounds.y + bounds.height) / Tile.TILEHEIGHT));
     }
-
-   private boolean checkRight(){
+    private boolean checkRight(){
         int tx = (int) (x + 3 + bounds.x + bounds.width) / Tile.TILEWIDTH;
         return (!collisionWithTile2(tx, (int) (y + bounds.y) / Tile.TILEHEIGHT) &&
                 !collisionWithTile2(tx, (int) (y + bounds.y + bounds.height) / Tile.TILEHEIGHT));
     }
-
-
-
 
     protected boolean collisionWithTile2(int x, int y) {
         return handler.getWorld().getTile(x,y).isSolid();
@@ -82,17 +73,12 @@ public class Enemy extends Creature {
     @Override
     public void render(Graphics graph){
 
-        graph.drawImage(Assets.enemy,(int)(x-handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width , height, null);
-        graph.setColor(Color.RED);
-        graph.fillRect((int)(x + bounds.x - handler.getGameCamera().getxOffset()),
-                (int)(y + bounds.y-handler.getGameCamera().getyOffset()),
-                bounds.width,bounds.height);
+        graph.drawImage(Assets.enemy,(int)( x-handler.getGameCamera().getxOffset() ), (int) ( y - handler.getGameCamera().getyOffset() ), width , height, null);
+        graph.setColor( Color.RED );
+        graph.fillRect((int)( x + bounds.x - handler.getGameCamera().getxOffset() ),
+                (int)( y + bounds.y-handler.getGameCamera().getyOffset() ),
+                bounds.width,bounds.height );
     }
-    public  float getXbound(){
-        return x + bounds.x-handler.getGameCamera().getxOffset();
-    }
-    private float getYbound(){
-        return y + bounds.y-handler.getGameCamera().getyOffset();
-    }
+
 }
 
