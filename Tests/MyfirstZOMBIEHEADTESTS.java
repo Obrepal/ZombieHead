ccc9a79db68c85e361b47d0146c36aaa9234dab0
Obrepal <48407260+Obrepal@users.noreply.github.com>
2019-12-09 20:22:31 +0100

    Adding tests

---
+++ Tests/MyfirstZOMBIEHEADTESTS.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyfirstZOMBIEHEADTESTS {
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+}
+
+
