1ad41be51f99f914b875613b6c40bd109768c968
Obrepal <48407260+Obrepal@users.noreply.github.com>
2019-12-09 21:08:25 +0100

    Adding tests

--- Tests/MyfirstZOMBIEHEADTESTS.java
+++ Tests/MyfirstZOMBIEHEADTESTS.java
@@ -1,12 +1,37 @@
+
+import com.game.MAIN.Handler;
+import com.game.enitity.creature.Player;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.awt.event.KeyEvent;
+
+
 public class MyfirstZOMBIEHEADTESTS {
+
+
 
     @Test
     public void firstTest() {
         Assert.assertTrue(true);
     }
+
+    @Test
+    public void testCollision(){
+
+    }
+    @Test
+    public void testKeyListener(){
+        Handler handler;
+        Player player = new Player(handler  ,100, 200);
+        KeyEvent e = new KeyEvent()
+
+
+
+
+    }
+
+
 }
 
 
