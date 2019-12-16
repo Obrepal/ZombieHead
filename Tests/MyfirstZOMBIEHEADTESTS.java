aa53b0d0299bc0ed966bfc63f805af9b4f8eacd9
Obrepal <48407260+Obrepal@users.noreply.github.com>
2019-12-16 23:55:02 +0100

    I finally did sth with mockitoooooo

--- Tests/MyfirstZOMBIEHEADTESTS.java
+++ Tests/MyfirstZOMBIEHEADTESTS.java
@@ -3,11 +3,18 @@ import com.game.MAIN.Handler;
 import com.game.enitity.creature.Player;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*
+
 
 import java.awt.event.KeyEvent;
 
 
 public class MyfirstZOMBIEHEADTESTS {
+    Handler handler =mock(Handler.class);
+    Player player = new Player(handler, 700, 800);
 
 
 
@@ -16,12 +23,12 @@ public class MyfirstZOMBIEHEADTESTS {
         Assert.assertTrue(true);
     }
 
-    @Test
+    /*@Test
     public void testCollision(){
 
     }
     @Test
-    public void testKeyListener(){
+   public void testKeyListener(){
         Handler handler;
         Player player = new Player(handler  ,100, 200);
         KeyEvent e = new KeyEvent()
@@ -29,9 +36,20 @@ public class MyfirstZOMBIEHEADTESTS {
 
 
 
+    }*/
+
+
+
+    protected int value1, value2;
+    @Test
+    public void testAdd(){
+        value1 = 3;
+        value2 = 3;
+        double result = value1 + value2;
+        Assert.assertTrue(result == 5);
     }
 
 
 }
 
-
+*/
